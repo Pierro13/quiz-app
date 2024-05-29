0aa25dfaface95c3648dd46bdba1bc9581dc1b37
Pierro13 <70155472+Pierro13@users.noreply.github.com>
2024-05-29 16:42:54 +0200

    début postman

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -7,7 +7,11 @@ CORS(app)
 @app.route('/')
 def hello_world():
 	x = 'world'
-	return f"Hello, {x} and jopel and Pierro"
+	return f"Hello, {x} and jopel and Pierro & MAX"
+
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
 
 if __name__ == "__main__":
     app.run()
