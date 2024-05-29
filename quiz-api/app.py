ffbfa94899ec5eee15fc5ce4f4d656587a9318b4
Pierro13 <70155472+Pierro13@users.noreply.github.com>
2024-05-29 15:18:44 +0200

    maj requirements

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	x = 'world'
-	return f"Hello, {x}"
+	return f"Hello, {x} and jopel and Pierro"
 
 if __name__ == "__main__":
     app.run()
