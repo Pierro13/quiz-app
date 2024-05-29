d031aaa25f8b7ea82567ed15243d0cb2b165bf2f
Pierro13 <70155472+Pierro13@users.noreply.github.com>
2024-05-29 19:09:18 +0200

    N°4 finto mtn : 5 - Mise en place de la stack EndPoint - BDD

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -6,12 +6,27 @@ CORS(app)
 
 @app.route('/')
 def hello_world():
-	x = 'world'
-	return f"Hello, {x} and jopel and Pierro & MAX"
+    x = 'world'
+    return f"Hello, {x} and jopel and Pierro & MAX"
 
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
-	return {"size": 0, "scores": []}, 200
+    return {"size": 0, "scores": []}, 200
+
+
+#b'\xd8\x17\x06PG\x92\x93\xc1.\x02\x01\xe5\xfd\xf4_@'
+
+@app.route('/login', methods=['POST'])
+def login():
+    payload = request.get_json()
+    password = payload.get('password')
+
+    if password == 'flask2023':
+        token = b'\xd8\x17\x06PG\x92\x93\xc1.\x02\x01\xe5\xfd\xf4_@'
+        token_str = token.hex()
+        return jsonify({"token": token_str}), 200
+    else:
+        return 'Unauthorized', 401
 
 if __name__ == "__main__":
     app.run()
