15a50b8a9d9d782a241003ed8a3b7bbe5fabd1d4
Pierro13 <70155472+Pierro13@users.noreply.github.com>
2024-06-02 12:54:32 +0200

    avancement 5.2 fini go 5.3

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,8 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
+import sqlite3
+import jwt_utils
 
 app = Flask(__name__)
 CORS(app)
+
+def get_db_connection():
+    connexion = sqlite3.connect('bdd.db')
+    connexion.row_factory = sqlite3.Row
+    return connexion
 
 @app.route('/')
 def hello_world():
@@ -19,14 +26,33 @@ def GetQuizInfo():
 @app.route('/login', methods=['POST'])
 def login():
     payload = request.get_json()
-    password = payload.get('password')
-
-    if password == 'flask2023':
-        token = b'\xd8\x17\x06PG\x92\x93\xc1.\x02\x01\xe5\xfd\xf4_@'
-        token_str = token.hex()
-        return jsonify({"token": token_str}), 200
+    password = payload['password']
+    if password == 'correct_password':
+        token = jwt_utils.build_token()
+        return {"token": token}, 200
     else:
         return 'Unauthorized', 401
+    
+
+@app.route('/questions', methods=['POST'])
+def create_question():
+    token = request.headers.get('Authorization')
+    if not token or not jwt_utils.verify_token(token):
+        return 'Unauthorized', 401
+
+    data = request.get_json()
+    title = data['title']
+    text = data['text']
+    image = data['image']
+    position = data['position']
+
+    connexion = get_db_connection()
+    cursor = connexion.cursor()
+    cursor.execute('INSERT INTO Questions (titre, texte, image, position) VALUES (?, ?, ?, ?)',(title, text, image, position))
+    connexion.commit()
+    connexion.close()
+
+    return {'id': cursor.lastrowid, 'title': title, 'text': text, 'image': image, 'position': position}, 201
 
 if __name__ == "__main__":
     app.run()
